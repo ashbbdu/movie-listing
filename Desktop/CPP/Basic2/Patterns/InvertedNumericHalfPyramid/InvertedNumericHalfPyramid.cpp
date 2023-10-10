c8f2691410e1c7b59cb8f5b4586448fedeb6816a
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2023-10-10 19:45:11 +0530

    wip

---
+++ Desktop/CPP/Basic2/Patterns/InvertedNumericHalfPyramid/InvertedNumericHalfPyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int n;
+    cout << "Enter a number !" << endl;
+    cin >> n;
+
+    for(int row = 0 ; row < n ; row= row +1){
+        for(int col = 0; col < n - row ; col= col +1) {
+            cout << col+1 << " " ;
+        }
+        cout << endl;
+    }
+
+    int i = (1,2,3);
+    cout << i << endl ;
+}
